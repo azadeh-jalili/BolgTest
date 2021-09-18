2e26749172dab853bc06fd6457d75772ae80ae90
azadeh-jalili <azadeh.jalili62@gmail.com>
2021-09-18 17:28:22 +0430

    create project and add new app

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index, name='starting_page'),
+    path('posts',views.posts, name='posts_page'),
+    path('posts/<slug:slug>',views.single_post, name='post_detail_page'),
+
+]
